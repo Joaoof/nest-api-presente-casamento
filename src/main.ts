7a29611cb5287151f053dbcfb7471673869fc1d9
Joaoof <joaodeus400@gmail.com>
2025-06-04 23:05:35 -0300

    fix: update CORS configuration to allow requests from any origin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // ou use '*' para permitir qualquer origem (apenas em dev!)
+    origin: '*', // ou use '*' para permitir qualquer origem (apenas em dev!)
     credentials: true, // se estiver usando cookies/autenticação
   });
 
